642a1797eab5d2fd0030a8e28dc0731af5fc29d3
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-07-04 13:20:49 +1000

    fix responsive issue on Profil menu

--- src/components/profil/TabsNavigation.jsx
+++ src/components/profil/TabsNavigation.jsx
@@ -2,7 +2,7 @@ import TabButton from "./TabButton";
 
 export const TabsNavigation = ({ activeTab, setActiveTab }) => (
   <div className="mb-8">
-    <div className="flex gap-2 p-1 bg-black/30 border border-white/10 rounded-xl w-fit">
+   <div class="flex flex-col sm:flex-row w-full sm:w-fit gap-2 p-1 bg-black/30 border border-white/10 rounded-xl">
       <TabButton
         data-cy="tab-overview"
         value="overview"
